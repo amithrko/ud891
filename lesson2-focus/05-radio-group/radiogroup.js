3821ce6a1800392e532289a27b8f2c37e01fc40b
amithrko <amithrko777@gmail.com>
2018-09-13 08:43:08 +0530

    Update radiogroup.js

--- lesson2-focus/05-radio-group/radiogroup.js
+++ lesson2-focus/05-radio-group/radiogroup.js
@@ -31,7 +31,11 @@
 
         e.preventDefault();
 
-        // This seems like a good place to do some stuff :)
+        if (this.focusedIdx === 0) {
+          this.focusedIdx = this.buttons.length - 1;
+        } else {
+          this.focusedIdx--;
+        }
 
         break;
 
@@ -42,14 +46,18 @@
 
         e.preventDefault();
 
-        // This seems like a good place to do some stuff :)
+        if (this.focusedIdx === this.buttons.length - 1) {
+          this.focusedIdx = 0;
+        } else {
+          this.focusedIdx++;
+        }
 
         break;
       }
 
     }
 
-    this.changeFocus(this.focusedIdx); // <-- Hmm, interesting...
+    this.changeFocus(this.focusedIdx);
   };
 
   RadioGroup.prototype.changeFocus = function(idx) {
