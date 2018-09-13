a7701500ccc3aaa2b83ece0ac382f3240894a5cb
amithrko <amithrko777@gmail.com>
2018-09-13 08:36:16 +0530

    Update main.js

--- lesson2-focus/03-managing-focus/scripts/main.js
+++ lesson2-focus/03-managing-focus/scripts/main.js
@@ -18,6 +18,14 @@ page('/:slug', function(context) {
   var newPage = document.querySelector('main [data-page='+slug+']');
   newMenuItem.classList.add('is-active');
   newPage.classList.add('is-active');
+  
+  if(isFirstPage)
+  {
+    isFirstPage=false;
+    return;
+  }
+  
+  newPage.querySelector(h2).focus();
 
 });
 
